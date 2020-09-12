7e1090e4ed820a8e57b56cfe7b105740902cda18
Amy Nguyen <amy.yay22@gmail.com>
2020-09-12 16:31:33 -0700

    Add webcam.

--- static/main.js
+++ static/main.js
@@ -28,3 +28,25 @@ function getUserText() {
 
 // TODO: Create a function for rendering the information on the DOM
 // once it is fetched from the API
+
+function stop () { 
+    var stream = video.srcObject; 
+    var tracks = stream.getTracks(); 
+    for (var i = 0; i < tracks.length; i++) { 
+        var track = tracks[i]; 
+        track.stop(); 
+    } 
+    video.srcObject = null; 
+} 
+function start () { 
+    var video = document.getElementById('video'), 
+        vendorUrl = window.URL || window.webkitURL; 
+    if (navigator.mediaDevices.getUserMedia) { 
+        navigator.mediaDevices.getUserMedia({ video: true }) 
+            .then(function (stream) { 
+                video.srcObject = stream; 
+            }).catch(function (error) { 
+                console.log("Something went wrong!"); 
+            }); 
+    } 
+} 
