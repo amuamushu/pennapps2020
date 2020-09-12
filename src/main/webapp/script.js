ce1e840c52f620ae8f61f9015ab3690248aca813
Amy Nguyen <amy.yay22@gmail.com>
2020-09-11 21:00:42 -0700

    Add algorithm for getting the last sentence of user's input.

---
+++ src/main/webapp/script.js
@@ -0,0 +1,27 @@
+
+let oldText;
+let lastPeriodIndex = -1;
+
+/**
+ * Gets the text the user has inputted.
+ * 
+ * <p>This gets called every time the textbox changes
+ */
+function getUserText() {
+  // This will be all the text the user has written so far.
+  // We would like to filter it to be the most recent words.
+  let userText =  document.getElementById('text-area').value;
+  // Only process the text by sentences.
+  if (userText[userText.length - 1] == '.') {
+      let lastSentence = userText.substring(lastPeriodIndex + 1);
+      lastPeriodIndex = userText.length - 1;
+      // We decided that sentences less than 3 characters in length
+      // are not important.
+      if (lastSentence.length < 3) {
+          return;
+      }
+      // TODO: Send a post request to a servlet in order to call the API
+      // on last sentence sentence.
+      console.log(lastSentence);
+  }
+}
